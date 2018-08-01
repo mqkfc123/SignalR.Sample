c4f96fd012bf5b6d1fed6dff3a6d49c68b218e40
liuyl <mqkfc123@163.com>
2018-08-01 10:39:33 +0800

    signalr

---
+++ src/SignalR.Sample/MyConnection1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using System.Diagnostics;
+
+namespace SignalR.Sample
+{
+    public class MyConnection1 : PersistentConnection
+    {
+        public MyConnection1()
+        {
+            Debug.WriteLine("构造函数初始化");
+        }
+
+        protected override Task OnConnected(IRequest request, string connectionId)
+        {
+            return Connection.Send(connectionId, "Welcome!");
+        }
+
+        protected override Task OnReceived(IRequest request, string connectionId, string data)
+        {
+            return Connection.Broadcast(data);
+        }
+
+        public override void Initialize(IDependencyResolver resolver)
+        {
+            Debug.WriteLine("Initialize");
+
+            base.Initialize(resolver);
+        }
+
+
+    }
+}
---
+++ src/SignalR.Sample/Startup1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SignalR.Sample.Startup1))]
+
+namespace SignalR.Sample
+{
+    public class Startup1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // 有关如何配置应用程序的详细信息，请访问 http://go.microsoft.com/fwlink/?LinkID=316888
+
+            //app.MapSignalR<MyConnection1>("/myconn");
+            //开启jsonp
+            //app.MapSignalR<MyConnection1>("/myconn", new Microsoft.AspNet.SignalR.ConnectionConfiguration()
+            //{
+            //    EnableJSONP = true
+            //});
+
+
+            app.Map("/myconn", (map) =>
+            {
+                map.RunSignalR<MyConnection1>(new Microsoft.AspNet.SignalR.HubConfiguration()
+                {
+                    //1. 开启jsonp 跨域
+                    EnableJSONP = true
+                });
+                //2.开启cors 跨域
+                //从nuget中获取一个中间件Microsoft.Owin.Cors 
+                map.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            });
+
+
+        }
+    }
+}
